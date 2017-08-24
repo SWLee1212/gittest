03154df2751ef042e868ed187063326a39d7d058
Sangwook Lee <swlee1212@gmail.com>
2017-08-24 18:07:49 +0900

    lp solver update

--- python_or/Gachon_CS50_OR_KMOOC-master/Gachon_CS50_OR_KMOOC-master/assignment/ps3/gachon_lp_solver.py
+++ python_or/Gachon_CS50_OR_KMOOC-master/Gachon_CS50_OR_KMOOC-master/assignment/ps3/gachon_lp_solver.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class GachonLPSolver:
     MAXIMIZE = 0
     MINIMIZE = 1
@@ -18,25 +19,73 @@ class GachonLPSolver:
         self._non_basic_variables_index = None
 
     def set_objective_variables(self, objective_coefficient_vector, optimize_setting):
-        self._objective_variables = objective_coefficient_vector
+        self._objective_variables = np.array(objective_coefficient_vector)
         self._optimize_setting = optimize_setting
+
 
     def add_constraints(self, constraints_coefficient_vector, sign, rhs):
         self._constraints_sign_list.append(sign)
-        temp = constraints_coefficient_vector + [rhs]
-        temp = np.array(temp)
+        temp_vector = np.array(constraints_coefficient_vector + [rhs])
 
         if self._constraints_coefficient_matrix is None:
-            self._constraints_coefficient_matrix = temp
+            self._constraints_coefficient_matrix = temp_vector
         else:
-            self._constraints_coefficient_matrix = np.vstack((self._constraints_coefficient_matrix,temp))
-
-        # print(self._constraints_coefficient_matrix)
+            self._constraints_coefficient_matrix = np.vstack([self._constraints_coefficient_matrix,
+                                                    temp_vector])
 
     def update(self):
-        pass
+        obj_var_len = self._objective_variables.shape[0]
+        cons_row_len = self._constraints_coefficient_matrix.shape[0]
+
+
+        strand_matrix_col_len  = 1 + obj_var_len + cons_row_len
+        strand_matrix_row_len= 1 + cons_row_len
+
+        for row_vector_index in range(strand_matrix_row_len) :
+            temp_vector = np.array([])
+
+            if row_vector_index == 0:
+                temp_vector = np.array( [1] + (-self._objective_variables).tolist() + [0] * (cons_row_len + 1))
+            else:
+                temp_vector = np.array(
+                                [1] +
+                                self._constraints_coefficient_matrix[row_vector_index - 1][0:-1].tolist() +
+                                np.eye(cons_row_len)[row_vector_index-1].tolist() +
+                                [self._constraints_coefficient_matrix[row_vector_index - 1][-1]]
+                                )
+
+            if self._standard_form_matrix is None:
+                self._standard_form_matrix = temp_vector
+            else:
+                self._standard_form_matrix = np.vstack([self._standard_form_matrix,
+                                                        temp_vector])
+
 
     def optimize(self):
+
+        self._basic_variables_index = None
+        self._non_basic_variables_index = None
+
+        # self._standard_form_matrix
+        # gauss_jordan_elimination_process()
+
+        # 초기화 nbs = s1, s2, ...  bs = x1,x2...
+
+        # entering variable 선정
+        # step 1 find pivot value col_position
+        # check first row 에서 최소값이 <0 인지 확인, 최소 값인 column_position 찾아야 함
+        # 만약 최소값이 0보다 크면 loop를 빠져 나감
+
+        # exit variable 선정
+        # step 2 find pivot value row_position
+        #        rhs/x1 한 값이 min인 row_index, 이 때 nbs, bs value 위치 저장
+
+        # step 3 pivot col_position, row_position에 대해 GEP 수행
+
+        # step 4 first row(obj vector) 에서 최소값이 <0 인지 확인, 최소값이 col_position 찾기
+        # 만약 최소값이 0보다 크면 loop를 빠져 나감
+
+
         pass
 
     def gauss_jordan_elimination_process(self, pivot_row_position, column_position):
